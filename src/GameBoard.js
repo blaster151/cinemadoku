bc2e494f899f96910b15e4a5a437b8f039ae586d
Jeff Benson <blaster151@gmail.com>
2024-10-15 21:54:17 -0400

    Initial setup with GameBoard and Tiles components

--- src/GameBoard.js
+++ src/GameBoard.js
@@ -1,1 +1,11 @@
- 
+import React from 'react';
+
+function GameBoard() {
+  return (
+    <div className="game-board">
+      <h2>GameBoard</h2>
+    </div>
+  );
+}
+
+export default GameBoard;
--- src/Tiles.js
+++ src/Tiles.js
@@ -1,1 +1,11 @@
- 
+import React from 'react';
+
+function Tiles() {
+  return (
+    <div className="tiles">
+      <h2>Tiles</h2>
+    </div>
+  );
+}
+
+export default Tiles;
